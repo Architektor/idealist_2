d3ffdabfb5314cf049027e880fc01fe05bf58ea1
Architektor <gloosx@gmail.com>
2016-05-21 01:18:30 +0300

    add task action

--- src/js/actions/TaskActions.js
+++ src/js/actions/TaskActions.js
@@ -1,22 +1,8 @@
 import * as types from '../constants/ActionTypes';
 
-export function addFriend(name) {
+export function addTask(task) {
   return {
-    type: types.ADD_FRIEND,
-    name
-  };
-}
-
-export function deleteFriend(id) {
-  return {
-    type: types.DELETE_FRIEND,
-    id
-  };
-}
-
-export function starFriend(id) {
-  return {
-    type: types.STAR_FRIEND,
-    id
+    type: types.ADD_TASK,
+    task: task
   };
 }
---
+++ src/js/components/TaskPage/TaskPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TaskPage extends React.Component {
+  render() {
+    return (
+      <div className={'task__page__wrapper'}>
+        <h1>task</h1>   
+      </div>
+    );
+  }
+}
