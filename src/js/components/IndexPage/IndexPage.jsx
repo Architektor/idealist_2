04ad59eebaaeb5e847596ce370e21496ccae08a0
Architektor <gloosx@gmail.com>
2016-05-20 22:48:09 +0300

    boiler commit

---
+++ src/js/components/IndexPage/IndexPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class IndexPage extends React.Component {
+  render() {
+    return (
+      <div className={'index__page'}>
+      <h1>
+        this is index page
+      </h1>
+      </div>
+    );
+  }
+}
